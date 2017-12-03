fef08229489f8a7ff27c1ab94a7777f3bbefdfcf
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-12-03 21:02:13 +0100

    Add images

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,15 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
+import Loser from '../images/loser.jpg'
+import Winner from '../images/winner.png'
 
 class Game extends PureComponent {
   render() {
     return (
       <div>
         <h1>{ this.props.game.wordToShow }</h1>
-        { this.props.game.isWinner ? <h1>Winner</h1> : null }
-        { this.props.game.gameOver ? <h1>Game over!</h1> : null }
+        { this.props.game.isWinner ? <h1><img class="winner" src={Winner} alt="winer" /></h1> : null }
+        { this.props.game.gameOver ? <h1><img class="loser" src={Loser} alt="loser" /></h1>: null }
         <span>Number of mistakes: { this.props.game.wrongGuessCount} / 6</span>
       </div>
     )
